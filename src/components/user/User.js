806af69b3f34cee0ab2bee401781bf6471cf8d47
Andreus75 <t_andriy@ukr.net>
2021-08-27 01:06:56 +0300

    create output address, company and geo

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -1,4 +1,3 @@
-
 export default function User ({user, history}) {
     const navigateTo = () => {
         history.push(`/users/${user.id}`);
@@ -7,8 +6,6 @@ export default function User ({user, history}) {
     return (
         <div>
             {user.id}. {user.name} - <button onClick={navigateTo}>details</button>
-            {/*{user.id}. {user.name} - {user.username}. Email - {user.email} -  <Link to={`/users/${user.id}`}>user detail</Link>*/}
-            {/*{user.id}. {user.name} - {user.username}. Email - {user.email} - <Link to={{pathname: `/users/${user.id}`, state: user}}>user detail</Link>*/}
         </div>
     );
 }
