f5ff15a2c4e442693e689124ed70bfc28fe93b43
Andreus75 <t_andriy@ukr.net>
2021-08-26 18:19:34 +0300

    create components Users, User, Posts, Post, Comments and Comment and service components for user, post and comment.And create components detail...

--- src/components/address/Address.js
+++ src/components/address/Address.js
@@ -3,15 +3,17 @@ import Geo from "../geo/Geo";
 export default function Address ({address}) {
     // let {street, suite, city, zipcode} = address;
     // console.log({address});
-    console.log({address});
+    console.log(address);
     return (
         <div>
             Address :
-            {/*Street : {street}*/}
-            {/*Suite : {suite}*/}
-            {/*City : {city}*/}
-            {/*Zipcode : {zipcode}*/}
-            {/*<Geo geo={geo}/>*/}
+            {address &&  <div>
+                Street : {address.street}
+                Suite : {address.suite}
+                City : {address.city}
+                Zipcode : {address.zipcode}
+                {/*<Geo geo={geo}/>*/}
+            </div>}
         </div>
     );
 }
--- src/components/company/Company.js
+++ src/components/company/Company.js
@@ -1,5 +1,5 @@
 export default function Company ({company}) {
-    console.log({company});
+    // console.log({company});
     return (
         <div>
 
